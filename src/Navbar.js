411c460da4ed0339de33b2e4c4e4fa8d22fdd34b
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-08-25 08:50:40 +0600

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Box, Flex, Heading, Spacer } from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+
+
+ const Navbar = ({ toggleColorFlag }) => {
+  return (
+    <Flex align="center" boxShadow="base" p="2" mb="2">
+    <Box   p="2">
+      <Heading display="flex" alignItems="center" size="lg" >E-buy Support </Heading>
+    </Box>
+    <Spacer />
+    <Box>
+      <ColorModeSwitcher toggleColorFlag={toggleColorFlag} />
+      {/* <ColorModeSwitcher /> */}
+    </Box>
+  </Flex>
+  )
+}
+
+export default Navbar
